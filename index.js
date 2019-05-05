e2404c085378bc0b10b8bf3f49d721de051b4883
Arafat Husayn <arafat@tuta.io>
2019-05-05 18:47:34 +0600

    new code to make it work again

--- index.js
+++ index.js
@@ -1,71 +1,68 @@
-/* eslint-disable */
-(function () {
+(function() {
+  function hideAds() {
+    var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]');
+    for (feed of feeds) {
+      var links = feed.querySelectorAll("div a");
+      for (link of links) {
+        if (/s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(link.innerText)) {
+          var p = link.closest('[data-testid*="fbfeed_story"]');
+          p.remove();
+        }
+      }
+    }
+  }
 
-	function hideAds() {
-		var feeds = document.querySelectorAll('[data-testid="fbfeed_story"]');
-		for ( feed of feeds ) {
-				var spans = feed.querySelectorAll('span');
-				for ( anySpan of spans ) {
-						if( anySpan.querySelector('a') ) {
-								if ( anySpan.querySelector('a').href.replace(window.location.href, '') === '#'
-								&& /s.?p.?o.?n.?s.?o.?r.?e.?d/gi.test(anySpan.innerText) ) {
-										var p = anySpan.closest('[data-testid="fbfeed_story"]');
-										if ( p.style.display !== "none" ) {
-											p.style.display = "none";
-										}
-								}
-						}
-				}
-		}
-	}
-
-	function hideAdsOnSides() {
-		if (document.querySelector(".ego_section")) {
+  function hideAdsOnSides() {
+    if (document.querySelector(".ego_section")) {
       document.querySelector(".ego_section").style.display = "none";
     }
     if (document.querySelector("#GroupsRHCSuggestionSection")) {
       document.querySelector("#GroupsRHCSuggestionSection").style.display = "none";
     }
-	}
+  }
 
-	function deferredHider(timeoutId) {
-		hideAds();
-		clearTimeout(timeoutId);
-	}
-	
-	var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver;
-	var container = document.documentElement || document.body;
-	var observer = new MutationObserver(function(mutations) {
-		var timeoutId4 = setTimeout(function() {
-			deferredHider(timeoutId4);
-		}, 1);    
-	});
-	var config = { attributes: false, childList: true, subtree: true };
-	observer.observe(container, config);
-
-	document.addEventListener("load", function(event) {
-		hideAds();
-		hideAdsOnSides();
-		var timeoutId = setTimeout(function() {
-			deferredHider(timeoutId);
-		}, 100);
-
-  }, { passive: true });
-	
-	document.addEventListener("DOMContentLoaded", function(event) {
-		hideAds();
-		hideAdsOnSides();
-		var timeoutId2 = setTimeout(function() {
-			deferredHider(timeoutId2);
-		}, 100);
-  }, { passive: true });
-	
-	container.addEventListener("click", function(event) {
+  function deferredHider(timeoutId) {
     hideAds();
-  }, { passive: true });
+    clearTimeout(timeoutId);
+  }
+
+  var MutationObserver = window.MutationObserver || window.webKitMutationObserver || window.MozMutationObserver;
+  var container = document.documentElement || document.body;
+
+  var observer = new MutationObserver(function(mutations) {
+    var timeoutId4 = setTimeout(function() {
+      deferredHider(timeoutId4);
+    }, 5);
+  });
+
+  var config = { attributes: false, childList: true, subtree: true };
+  observer.observe(container, config);
+
+  document.addEventListener(
+    "load",
+    function(event) {
+      hideAds();
+      hideAdsOnSides();
+      var timeoutId = setTimeout(function() {
+        deferredHider(timeoutId);
+      }, 100);
+    },
+    { passive: true }
+  );
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function(event) {
+      hideAds();
+      hideAdsOnSides();
+      var timeoutId2 = setTimeout(function() {
+        deferredHider(timeoutId2);
+      }, 100);
+    },
+    { passive: true }
+  );
 
   var timeoutId3 = setTimeout(function() {
-		deferredHider(timeoutId3);
-	}, 1000);
-	
+    deferredHider(timeoutId3);
+  }, 1000);
 })();
